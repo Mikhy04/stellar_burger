d775b979aed60e2c668c6fe7515666fff0d67c69
Mikhy04 <Mikhy04@yandex.ru>
2025-05-05 19:13:56 +0700

    Сборка и настройка webpack, react

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import React from 'react';
+import React, { JSX } from 'react';
 import { BrowserRouter, Routes, Route, Navigate, Outlet } from 'react-router-dom';
-import { Modal } from './components/Modal/Modal';
+import { Modal } from '../modal/modal';
 import { ConstructorPage } from '@pages';
 import { AppHeader } from '@components';
 import '../../index.css';
 import styles from './app.module.css';
-import Feed from '@pages/Feed';
-import Login from '@pages/Login';
-import Register from '@pages/Register';
-import ForgotPassword from '@pages/ForgotPassword';
-import ResetPassword from '@pages/ResetPassword';
-import Profile from '@pages/Profile';
-import ProfileOrders from '@pages/ProfileOrders';
-import NotFound404 from '@pages/NotFound404';
-import OrderInfo from './components/OrderInfo/OrderInfo';
-import IngredientsDetails from './components/IngredientsDetails/IngredientsDetails';
-import { ConstructorPage } from '@pages';
+import { Feed } from '@pages';
+import { Login } from '@pages';
+import { Register } from '@pages';
+import { ForgotPassword } from '@pages';
+import { ResetPassword } from '@pages';
+import { Profile } from '@pages';
+import { ProfileOrders } from '@pages';
+import { NotFound404 } from '@pages';
+import { OrderInfo } from '@components';
+import { IngredientDetails } from '@components';
 import '../../index.css';
-import styles from './app.module.css';
 
-import { AppHeader } from '@components';
 
 // Функция для проверки авторизации (замените на вашу логику)
 const isLoggedIn = () => {
@@ -28,7 +24,6 @@ const isLoggedIn = () => {
   return localStorage.getItem('token') !== null; 
 };
 
-// Компонент защищенного роута
 const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
   return isLoggedIn() ? children : <Navigate to="/login" />;
 };
@@ -56,16 +51,22 @@ const App = () => (
       <Route path="/" element={<AppLayout />} > {/* AppHeader будет общим для "/" и "/feed" */}
         <Route index element={<ConstructorPage />} /> {/* index для "/" внутри AppLayout */}
         <Route path="feed" element={<Feed />} />
-        <Route path="feed/:number" element={<Modal><OrderInfo /></Modal>} />
+        <Route path="feed/:number" element={<Modal title={''} onClose={function (): void {
+          throw new Error('Function not implemented.');
+        } }><OrderInfo /></Modal>} />
       </Route> {/* Закрываем роут AppLayout */}
-      <Route path="/ingredients/:id" element={<Modal><IngredientsDetails /></Modal>} />
+      <Route path="/ingredients/:id" element={<Modal title={''} onClose={function (): void {
+        throw new Error('Function not implemented.');
+      } }><IngredientDetails /></Modal>} />
       <Route path="/login" element={<ProtectedRoute><Login /></ProtectedRoute>} />
       <Route path="/register" element={<ProtectedRoute><Register /></ProtectedRoute>} />
       <Route path="/forgot-password" element={<ProtectedRoute><ForgotPassword /></ProtectedRoute>} />
       <Route path="/reset-password" element={<ProtectedRoute><ResetPassword /></ProtectedRoute>} />
       <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
       <Route path="/profile/orders" element={<ProtectedRoute><ProfileOrders /></ProtectedRoute>} />
-      <Route path="/profile/orders/:number" element={<ProtectedRoute><Modal><OrderInfo /></Modal></ProtectedRoute>} />
+      <Route path="/profile/orders/:number" element={<ProtectedRoute><Modal title={''} onClose={function (): void {
+        throw new Error('Function not implemented.');
+      } }><OrderInfo /></Modal></ProtectedRoute>} />
       <Route path="*" element={<NotFound404 />} />
     </Routes>
   </BrowserRouter>
--- webpack.config.js
+++ webpack.config.js
@@ -89,6 +89,8 @@ module.exports = {
     static: path.join(__dirname, './dist'),
     compress: true,
     historyApiFallback: true,
+    open: true,
+    hot: true,
     port: 4000
   }
 };
